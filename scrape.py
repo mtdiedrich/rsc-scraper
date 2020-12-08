bce594b722ac1de0630807cf778b853c792a8620
mtdiedrich <mitchdiedrich@gmail.com>
2020-12-07 20:29:21 -0600

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -4,29 +4,38 @@ import requests
 import tqdm
 import time
 
-"""
-If you need help with usage, you can message me (mitchy.#6281)
-I might not respond because that's just how I am.
-I may or may not update this if/when it gets broken by TRN.
-I may or may not update this to be of a higher quality.
-Licensing is more effort than this is worth. Anyone can use this for whatever.
-"""
 
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 255)
 
 
+"""
+If you need help with usage, you can message me (mitchy.#6281)
+I might not respond because that's just how I am.
+I may or may not update this if/when it gets broken by TRN.
+I may or may not update this to be of a higher quality.
+I may or may nut add instructions to the README.
+Licensing is more effort than this is worth. Anyone can use this for whatever.
+I think that error rows mean that that account hasn't been played on this season.
+"""
+
+
 def get_players_mmr(df):
     data = []
     for row in tqdm.tqdm(df.values):
-        mmr_data = list(row) + list(parse_page(row))
+        try:
+            mmr_data = list(row) + list(parse_page(row))
+        except:
+            mmr_data = list(row) + ['ERROR' for e in range(18)]
+            print('Link error: ', row[2])
+            print('Does this TRN page not have tournament data?')
         data.append(mmr_data)
     df = pd.DataFrame(data)
-    df.columns = ['Player', 'TRN Link', 'Unranked MMR', 'Unranked GP',
-            '1v1 MMR', '1v1 GP', '2v2 MMR', '2v2 GP', '3v3 MMR', '3v3 GP', 
-            'Hoops MMR', 'Hoops GP', 'Rumble MMR', 'Rumble GP', 
-            'Dropshot MMR', 'Dropshot GP', 'Snowday MMR', 'Snowday GP', 
-            'Tournament MMR', 'Tournament GP']
+    df.columns = ['RSC ID', 'Player', 'TRN Link', 'Unranked MMR', 
+            'Unranked GP', '1v1 MMR', '1v1 GP', '2v2 MMR', '2v2 GP', 
+            '3v3 MMR', '3v3 GP', 'Hoops MMR', 'Hoops GP', 'Rumble MMR',
+            'Rumble GP', 'Dropshot MMR', 'Dropshot GP', 'Snowday MMR',
+            'Snowday GP', 'Tournament MMR', 'Tournament GP']
     df = df.drop([c for c in df.columns if 'Unranked' in c], axis=1)
     return df
 
@@ -36,18 +45,19 @@ def get_display_value(string, value_name):
     loc = string.find(use_name)
     value_str = string[loc:]
     value_loc = value_str.find('displayValue')
-    value = value_str[value_loc:].split('":"')[1].split('","')[0]
+    value = value_str[value_loc:]
+    value = value.split('":"')[1].split('","')[0]
     return value
 
 
 def parse_page(row):
-    link = row[1]
-    name = row[0]
+    link = row[2]
+    name = row[1]
     page = requests.get(link)
     soup = BeautifulSoup(page.text, 'lxml')
     lines = [l.parent for l in soup.find_all('script')]
     data_str = [str(l) for l in lines if 'INITIAL_STATE' in str(l)][0]
-    data_str = data_str.replace('"name"', '[player name]')
+    data_str = data_str.replace(name, '[player name]')
     json_str = data_str.split('INITIAL_STATE')[1].replace('{}', 'null')
     json_list = json_str.split('"type":"playlist"')[1:10]
     data = []
@@ -61,7 +71,7 @@ def parse_page(row):
 
 def run():
     csv_loc = 'players.csv'
-    df = pd.read_csv(csv_loc)
+    df = pd.read_csv(csv_loc, header=None)
     mmr_df = get_players_mmr(df)
     print(mmr_df)
 
